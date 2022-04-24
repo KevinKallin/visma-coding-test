13202f0662bdedbcc97b404e70b071ef246f27bf
kevinkallin <kevin.kallin@hotmail.se>
2022-04-24 15:47:58 +0200

    chore: Added Customer and Product models

---
+++ VismaCodingTest/Shared/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace VismaCodingTest.Shared
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public Guid CustomerId { get; set; } = Guid.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        public string ContactName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool HasRebate { get; set; }
+
+    }
+}
---
+++ VismaCodingTest/Shared/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace VismaCodingTest.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal StandardPrice { get; set; }
+        public decimal AdjustedPrice { get; set; }
+
+    }
+}
