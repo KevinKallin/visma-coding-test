c6c6b98248f0d7196fe9c87124e4bd7ed9b75e1a
kevinkallin <kevin.kallin@hotmail.se>
2022-04-24 22:17:25 +0200

    chore: Minor changes

--- VismaCodingTest/Shared/Product.cs
+++ VismaCodingTest/Shared/Product.cs
@@ -3,10 +3,11 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public Category? Category { get; set; }
         public decimal StandardPrice { get; set; }
-        public decimal AdjustedPrice { get; set; }
-
+        public decimal AdjustedPrice =>
+            Math.Round(StandardPrice * 0.8M, 2);
     }
 }
