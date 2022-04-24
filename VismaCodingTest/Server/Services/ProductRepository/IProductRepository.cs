20842e6f2ee692ac5887e548d613d18fd6548276
kevinkallin <kevin.kallin@hotmail.se>
2022-04-24 15:52:24 +0200

    feat: Added functionality for viewing products

---
+++ VismaCodingTest/Server/Services/ProductRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using VismaCodingTest.Shared;
+
+namespace VismaCodingTest.Server.Services.ProductRepository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+    }
+}
---
+++ VismaCodingTest/Server/Services/ProductRepository/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using VismaCodingTest.Shared;
+
+namespace VismaCodingTest.Server.Services.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public List<Product> _productList = new List<Product>()
+        {
+            new Product
+            {
+                Id = 1,
+                Title = "Trotten",
+                Description = "Det här robusta skrivbordet klarar garanterat många år av kaffedrickande och hårt arbete.",
+                StandardPrice = 1795.90m,
+                AdjustedPrice = 1795.90m * 0.8m
+            },
+
+              new Product
+            {
+                Id = 2,
+                Title = "Botten",
+                Description = "Denna stolen kommer hjälpa att stötta din rygg med mycket stilla sittande",
+                StandardPrice = 493.90m,
+                AdjustedPrice = 493.90m * 0.8m
+            }
+        };
+
+        public IEnumerable<Product> GetProducts()
+        {
+            var list = _productList.ToList();
+            return list;
+        }
+
+    }
+}
